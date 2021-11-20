6a5610ecaedf971b8a806c856f06e9dd09046627
MoheeDG23 <mohee.datta@research.iiit.ac.in>
2021-11-20 22:28:23 +0530

    rr code updated

--- rr.cpp
+++ rr.cpp
@@ -20,34 +20,10 @@ void reorder(vector<int> &a, vector<int> &b){
 	}
 }
 
-int main()
+void round_robin(vector<int> &arrival_time, vector<int> &burst_time,vector<int> &completion_time, int time_quant)
 {
-    int num_proc;
-    cout<<"Enter no. of processes: ";cin>>num_proc;cout<<endl;
-
-    vector<int> arrival_time(num_proc);
-    vector<int> burst_time(num_proc);
-    vector<int> bt1(num_proc);
-    vector<int> completion_time(num_proc);
-
-    int idx, time_quant, c, r=0, f=0;
-    int rq[100][2];
-
-    cout<<"Enter time quantum: ";cin>>time_quant;cout<<endl;
-
-    for(int i=0; i<num_proc; i++)
-    {
-        cout<<"For Process "<<i+1<<":"<<endl;
-        cout<<"Arrival Time: ";cin>>arrival_time[i];cout<<endl;
-        cout<<"Burst Time: ";cin>>burst_time[i];cout<<endl;
-    }
-
-    cout<<endl<<endl;
-
-    reorder(arrival_time, burst_time);
-
-    bt1=burst_time;
-
+    int idx, c, r=0, f=0, rq[100][2];
+    int num_proc=arrival_time.size();
 
     if(burst_time[0]>time_quant){
 		c=arrival_time[0]+time_quant;
@@ -126,6 +102,12 @@ int main()
 		}
 	}
 
+    
+}
+
+pair<double, double> calculate(vector<int> &arrival_time, vector<int> &burst_copy,vector<int> &completion_time)
+{
+    int num_proc=arrival_time.size();
     vector<int> tat(num_proc), wait_time(num_proc);
     double avg_tat=0.0, avg_wait=0.0;
 
@@ -133,7 +115,7 @@ int main()
     for(int i=0; i<num_proc; i++)
     {
         tat[i]=completion_time[i]-arrival_time[i];
-        wait_time[i]=tat[i]-bt1[i];
+        wait_time[i]=tat[i]-burst_copy[i];
         avg_tat+=tat[i];
         avg_wait+=wait_time[i];
     }
@@ -141,11 +123,60 @@ int main()
     avg_wait/=num_proc;
     avg_tat/=num_proc;
 
-    cout<<"Avg TAT: "<<avg_tat<<endl<<"Avg Wait Time: "<<avg_wait<<endl;
+    pair<double,double> ans;
+    ans={avg_tat,avg_wait};
 
     cout<<endl<<"at     bt      ct       tat    wt"<<endl;
     for(int i=0; i<num_proc; i++)
     {
-        printf("%d\t%d\t%d\t%d\t%d\n",arrival_time[i],bt1[i],completion_time[i],tat[i],wait_time[i]);
+        printf("%d\t%d\t%d\t%d\t%d\n",arrival_time[i],burst_copy[i],completion_time[i],tat[i],wait_time[i]);
     }
+
+    return ans;
+}
+
+
+int main()
+{
+    int num_proc;
+    cout<<"Enter no. of processes: ";cin>>num_proc;cout<<endl;
+
+    vector<int> arrival_time(num_proc);
+    vector<int> burst_time(num_proc);
+    vector<int> burst_copy(num_proc);
+    vector<int> completion_time(num_proc);
+
+    int time_quant; 
+
+    cout<<"Enter time quantum: ";cin>>time_quant;cout<<endl;
+    if(time_quant<1) 
+    {
+        cout<<"time quantum should be a positive integer"<<endl;
+        return 0;
+    }
+
+    for(int i=0; i<num_proc; i++)
+    {
+        cout<<"For Process "<<i+1<<":"<<endl<<endl;
+        cout<<"Arrival Time: ";cin>>arrival_time[i];
+        cout<<"Burst Time: ";cin>>burst_time[i];cout<<endl;
+    }
+
+    cout<<endl<<endl;
+
+    reorder(arrival_time, burst_time);
+
+    burst_copy=burst_time;
+
+    round_robin(arrival_time, burst_time, completion_time, time_quant);
+    pair<double, double> results = calculate(arrival_time, burst_copy, completion_time);
+
+    double avg_tat, avg_wait;
+
+    avg_tat=results.first;
+    
+
+    cout<<"Avg TAT: "<<avg_tat<<endl<<"Avg Wait Time: "<<avg_wait<<endl;
+
+    
 }
